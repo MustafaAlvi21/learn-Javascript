bd367656078ab1017a60dc6070adbb0798c1d744
MustafaAlvi21 <mustafaalvi21@gmail.com>
2020-12-25 17:21:27 +0500

    class 1 on 25/12/2020

---
+++ class_1.js
@@ -0,0 +1,76 @@
+/* --------------------------- */
+/*         Print Method        */
+/* --------------------------- */
+
+// console.log('Hello beautiful')
+// console.log('32')
+// console.log(123)
+
+
+/* --------------------------- */
+/*     Scope Of Variables      */
+/* --------------------------- */
+
+// var name = 'Mustafa'
+// console.log('name 1 => ' + name)
+// var name = 'Ali'
+// console.log('name 2 => ' + name)
+
+// let name = 'Mustafa'
+// console.log('name 1 => ' + name)
+// let name = 'Ali'
+// console.log('name 2 => ' + name)
+
+// const name = 'Mustafa'
+// console.log('const name 1 =>' + name)
+// name = 'ali'
+
+
+/* --------------------------- */
+/*     Creating Marksheet      */
+/* --------------------------- */
+
+    // console.log('\n ------------  Mark Sheet ---------------\n')
+
+    // name = 'Mustafa'
+    // sec = 'B'
+    // roll_no = 216
+    // eng_marks = 90
+    // urduMarks = 80
+    // mathmarks = 60
+    // total_Marks = 100 + 100 + 100
+    // total_obtained = eng_marks + urduMarks + mathmarks
+    // percentage = (total_obtained * 100) /total_Marks 
+
+
+    // console.log('name : ' + name)
+    // console.log('sec : ' + sec)
+    // console.log('roll_no : ' + roll_no)
+    // console.log('eng_marks : ' + eng_marks)
+    // console.log('urduMarks : ' + urduMarks)
+    // console.log('mathmarks : ' + mathmarks)
+    // console.log('total_obtained : ' + total_obtained)
+    // console.log('percentage : ' + percentage)
+
+
+
+/* --------------------------- */
+/*         Caluclator          */
+/* --------------------------- */
+
+num1 = 5
+num2 = 10
+num3 = 20
+
+add = num1 + num2
+sub = num2 - num3
+mul = num2 * num1
+div = num3 / num1
+
+div = div + num3
+console.log('add => ' + add)
+console.log('sub => ' + sub)
+console.log('mul => ' + mul)
+console.log('div => ' + div)
+
+
