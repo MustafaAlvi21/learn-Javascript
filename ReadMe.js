0e1c1f5d2b12f15342e0d599995c08c6394d0392
MustafaAlvi21 <mustafaalvi21@gmail.com>
2021-01-09 23:42:51 +0500

    ReadMe

---
+++ ReadMe.js
@@ -0,0 +1,32 @@
+All recorded tutorials are available on the below link
+
+https://drive.google.com/drive/folders/1rO86yj0OAClc9bw5M5t89XE5eKXQT-dQ?usp=sharing
+
+class 2 task:
+
+Q1) Create a program & iterate from 0 to 20 & checks if the current number is even or odd and print all even numbers
+    the sample result is:
+
+    2 is even 
+    11 is odd 
+    16 is even
+
+    
+class 3 task:
+
+Q1) write a program for assigning grades
+    takes 1 argument, a total obtained marks.
+    prints the grade for marks, either "A", "B", "C", "D", or "F".
+
+Q2) Create an array of objects to hold information on your favorite recipes.
+    properties should be title (a string), servings (a number), and ingredient (a string).
+    Iterate through the array and the sample result is:
+
+Mole
+Servings: 2
+Ingredients:
+cumin
+cinnamon
+cocoa
+
+
